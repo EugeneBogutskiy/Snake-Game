d56a13d0b59d21a123f6ecfe47fb0c704daa598f
EugeneBogutskiy <bogutskiy1983@gmail.com>
2020-03-03 00:13:24 +0300

    Add Scripts

---
+++ Assets/Scripts/GameAssets.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class GameAssets : MonoBehaviour
+{
+    public static GameAssets gameAssets;
+
+    void Awake()
+    {
+        gameAssets = this;
+    }
+
+    public Sprite snakeHeadSprite;
+}
---
+++ Assets/Scripts/GameHandler.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class GameHandler : MonoBehaviour
+{
+    void Start()
+    {
+        GameObject snakeHeadGameObject = new GameObject();
+        SpriteRenderer snakeSpriteRenderer = snakeHeadGameObject.AddComponent<SpriteRenderer>();
+        snakeSpriteRenderer.sprite = GameAssets.gameAssets.snakeHeadSprite;
+    }
+
+    void Update()
+    {
+        
+    }
+}

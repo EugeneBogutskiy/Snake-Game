4027d6ede3da3952573ad88aa2d40e2eb1de15bf
EugeneBogutskiy <bogutskiy1983@gmail.com>
2020-03-05 00:20:26 +0300

    Script for spawn food e.c.

---
+++ Assets/Scripts/LevelGrid.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class LevelGrid
+{
+    Vector2Int foodGridPosition;
+    int width;
+    int height;
+
+    public LevelGrid(int width, int height)
+    {
+        this.width = width;
+        this.height = height;
+
+        SpawnFood();
+    }
+    void SpawnFood()
+    {
+        foodGridPosition = new Vector2Int(Random.Range(0, width), Random.Range(0, height));
+
+        GameObject foodGameObject = new GameObject("Food", typeof(SpriteRenderer));
+        foodGameObject.GetComponent<SpriteRenderer>().sprite = GameAssets.gameAssets.foodSprite;
+
+        foodGameObject.transform.position = new Vector3(foodGridPosition.x, foodGridPosition.y);
+    }
+}

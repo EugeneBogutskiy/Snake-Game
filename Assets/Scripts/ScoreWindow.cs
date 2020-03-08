321250871a7131866199df2f8f9ae3507398572a
EugeneBogutskiy <bogutskiy1983@gmail.com>
2020-03-09 00:14:40 +0300

    Count Score and shows it on displey

---
+++ Assets/Scripts/ScoreWindow.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreWindow : MonoBehaviour
+{
+    Text scoreText;
+
+    void Awake()
+    {
+        scoreText = transform.Find("ScoreText").GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        scoreText.text = GameHandler.GetScore().ToString();
+    }
+}

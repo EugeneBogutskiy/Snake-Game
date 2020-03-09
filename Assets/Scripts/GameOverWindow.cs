8d95366624b8e0554fdf9f35afba6948a92ba753
EugeneBogutskiy <bogutskiy1983@gmail.com>
2020-03-10 00:44:55 +0300

    GameOverWindow script with retry button

---
+++ Assets/Scripts/GameOverWindow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOverWindow : MonoBehaviour
+{
+    void Awake()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    public void SetWindowActive()
+    {
+        this.gameObject.SetActive(true);
+    }
+
+    public void RetryGame()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+}

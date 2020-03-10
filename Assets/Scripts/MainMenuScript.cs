1d40fc2fc892e228c85646fb8a4e2da78c6c7615
EugeneBogutskiy <bogutskiy1983@gmail.com>
2020-03-11 00:36:50 +0300

    MeinMenuScript for Main Menu Start/Quit

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+    
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}

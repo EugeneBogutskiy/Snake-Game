62ceea145da98835f5ebf031234d7d1451413218
EugeneBogutskiy <bogutskiy1983@gmail.com>
2020-03-14 00:30:35 +0300

    It keeps high score in sistem

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public static class Score
+{
+    public static int GetHighScore()
+    {
+        return PlayerPrefs.GetInt("highScore", 0);
+    }
+
+    public static bool TrySetNewHighScore(int score)
+    {
+        int highScore = GetHighScore();
+
+        if (score > highScore)
+        {
+            PlayerPrefs.SetInt("highScore", score);
+            PlayerPrefs.Save();
+            return true;
+        }
+        else return false;
+    }
+}

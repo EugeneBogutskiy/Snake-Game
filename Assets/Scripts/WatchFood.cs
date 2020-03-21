038fa1744879979262f4fa01e96be4c662c09783
Eugene <59443750+EugeneBogutskiy@users.noreply.github.com>
2020-03-22 00:43:45 +0300

    Add files via upload

---
+++ Assets/Scripts/WatchFood.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class WatchFood : MonoBehaviour
+{
+    void Update()
+    {
+        LookAtFood();
+    }
+
+    private void LookAtFood()
+    {
+        GameObject food = GameObject.Find("Food");
+        if (food != null)
+        {
+            Vector2 direction = new Vector2(food.transform.position.x - transform.position.x, food.transform.position.y - transform.position.y);
+            transform.up = -direction;
+        }
+    }
+}
